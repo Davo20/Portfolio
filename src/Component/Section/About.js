306ace2f909b7592e7efd2c40bc8b1ff34aceae0
Davo20 <dav507805@gmail.com>
2022-06-30 20:17:06 +0400

    My Portfolio

--- src/Component/Section/About.js
+++ src/Component/Section/About.js
@@ -16,7 +16,7 @@ export default function About({lang, fon}){
                 <div data-aos="fade-right" data-aos-offset="100" data-aos-duration="1000" className="aboutImg">
                 </div>
                 <div data-aos="fade-left" data-aos-offset="95" data-aos-duration="1000" className="aboutText">
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae neque ratione sapiente unde quidem! Dolor, blanditiis tempora doloribus sequi beatae quod maxime rerum soluta voluptas cumque doloremque aliquam obcaecati dolores?</p>
+                    <p>{lang.aboutMe}</p>
                 </div>
             </div>
         </section>
